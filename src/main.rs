8b7acfd465ecb133a5a6cd4304915fba0125ae36
Krishna Ramasimha <krishnaramasimha12@gmail.com>
2021-06-21 12:31:54 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,21 @@ use std::io::prelude::*;
 
 #[derive(Logos, Debug, PartialEq)]
 enum Token {
-    // Tokens can be literal strings, of any length.
     #[token("fast")]
     Fast,
 
     #[token(".")]
     Period,
 
-    // Or regular expressions.
+    
     #[regex("[a-zA-Z]+")]
     Text,
 
     #[regex("[1-9]+")]
     Number,
-
-    // Logos requires one token variant to handle errors,
-    // it can be named anything you wish.
+    
     #[error]
-    // We can also use this variant to define whitespace,
-    // or any other matches we wish to skip.
+   
     #[regex(r"[ \t\n\f]+", logos::skip)]
     Spaces,
 }

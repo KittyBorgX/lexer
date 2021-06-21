d0d5e15f4cf7fff0e88c3c46076a44b6a8e3210a
Krishna Ramasimha <krishnaramasimha12@gmail.com>
2021-06-21 12:31:07 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,35 +35,4 @@ fn main() -> std::io::Result<()> {
         dbg!(token);
     })
 }
-// fn main() {
-
-    // hi();
-
-    // let mut lex = Token::lexer("Create ridiculously fast Lexers.");
-    // for token in Token::lexer("ABC DEF 123") {
-    //     dbg!(token);
-    // }
-    // assert_eq!(lex.next(), Some(Token::Text));
-    // assert_eq!(lex.span(), 0..6);
-    // assert_eq!(lex.slice(), "Create");
-
-    // assert_eq!(lex.next(), Some(Token::Text));
-    // assert_eq!(lex.span(), 7..19);
-    // assert_eq!(lex.slice(), "ridiculously");
-
-    // assert_eq!(lex.next(), Some(Token::Fast));
-    // assert_eq!(lex.span(), 20..24);
-    // assert_eq!(lex.slice(), "fast");
-
-    // assert_eq!(lex.next(), Some(Token::Text));
-    // assert_eq!(lex.span(), 25..31);
-    // assert_eq!(lex.slice(), "Lexers");
-
-    // assert_eq!(lex.next(), Some(Token::Period));
-    // assert_eq!(lex.span(), 31..32);
-    // assert_eq!(lex.slice(), ".");
-
-    // assert_eq!(lex.next(), None);
-// }
-
 
